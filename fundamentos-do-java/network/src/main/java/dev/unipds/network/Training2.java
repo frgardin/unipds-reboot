2de50e8b89f28610d7213f9debc2c7d30b3a9b2b
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-04-09 23:55:27 -0300

    training2

---
+++ fundamentos-do-java/network/src/main/java/dev/unipds/network/Training2.java
@@ -0,0 +1,22 @@
+package dev.unipds.network;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class Training2 {
+    
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("https://viacep.com.br/ws/01001000/json/");
+        
+        InputStream inputStream = url.openStream();
+        Scanner sc = new Scanner(inputStream);
+
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+
+        sc.close();
+    }
+}
+
