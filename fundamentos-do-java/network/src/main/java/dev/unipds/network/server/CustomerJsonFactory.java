7d2a23f57d974cfcea3d6efae49a935e4a4f3132
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-04-11 11:22:55 -0300

    feat: add create and delete method for file creation

--- fundamentos-do-java/network/src/main/java/dev/unipds/network/server/CustomerJsonFactory.java
+++ fundamentos-do-java/network/src/main/java/dev/unipds/network/server/CustomerJsonFactory.java
@@ -1,7 +1,10 @@
 package dev.unipds.network.server;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.FileSystems;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -10,14 +13,29 @@ import dev.unipds.network.database.mock.DatabaseMock;
 
 public class CustomerJsonFactory {
 
-    public static void main(String[] args) throws Exception {
+    public static Path createSampleJson() throws Exception {
         var database = new DatabaseMock(4);
         var customers = database.getCustomers();
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json = gson.toJson(customers);
-        
+
         Path path = Path.of("" + System.currentTimeMillis() + "-customers.json");
         Files.writeString(path, json);
+        return path;
+    }
+
+    public static void deleteAll() throws IOException {
+        PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:*-customers.json");
+        try (var stream = Files.list(Path.of("."))) {
+            stream.filter(p -> matcher.matches(p.getFileName()))
+                    .forEach(p -> {
+                        try {
+                            Files.delete(p);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+        }
     }
 }
