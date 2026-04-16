17a029d5822039200bb7675a66466788691a1add
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-04-15 23:53:01 -0300

    remove unecessary method

--- fundamentos-do-java/jdbc/app/src/main/java/com/felipegardin/model/Categoria.java
+++ fundamentos-do-java/jdbc/app/src/main/java/com/felipegardin/model/Categoria.java
@@ -1,17 +1,8 @@
 package com.felipegardin.model;
 
-import java.util.Arrays;
-
 public enum Categoria {
     ENTRADAS,
     PRATOS_PRINCIPAIS,
     BEBIDAS,
     SOBREMESA;
-
-    public static Categoria from(String categoria) {
-        return Arrays.stream(values())
-                        .filter(c -> c.name().equalsIgnoreCase(categoria))
-                        .findFirst()
-                        .orElseThrow(() -> new IllegalStateException("Esta categoria nao eh suportada."));
-    }
 }
