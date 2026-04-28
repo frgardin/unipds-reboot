1d2258bed961bcba2ef9ac2f54fef507e02c1359
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-04-27 23:14:25 -0300

    refactor: remove product endpoint from HelloController
    Removed the `POST /product` endpoint and its related imports (`PostMapping`, `RequestBody`, and `Product`) from `HelloController`, simplifying the controller to only handle the basic "Hello World" GET request.

--- mod2/spring/helloworld/src/main/java/br/com/felipegardin/helloworld/controller/HelloController.java
+++ mod2/spring/helloworld/src/main/java/br/com/felipegardin/helloworld/controller/HelloController.java
@@ -1,11 +1,7 @@
 package br.com.felipegardin.helloworld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import br.com.felipegardin.helloworld.domain.Product;
 
 @RestController
 public class HelloController {
@@ -14,11 +10,4 @@ public class HelloController {
     public String sayHello() {
         return "Hello World";
     }
-
-    @PostMapping("/product")
-    public Product addNewProduct(@RequestBody Product product) {
-        System.out.println("Product received");
-        return product;
-    } 
-
 }
